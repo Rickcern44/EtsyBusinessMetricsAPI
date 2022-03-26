8dd3caf40ecc95a189f1d3c5d090444e6fda1122
Ricky Cerny <Cerny.Ricky@yahoo.com>
2022-03-26 07:48:49 -0400

    Committing from dev

---
+++ EtsyBusinessMetricsAPI/Models/EstySale.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EtsyBusinessMetricsAPI.Models
+{
+
+    public class Result
+    {
+        public long transaction_id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public string currency_code { get; set; }
+        public int quantity { get; set; }
+        public string shipping_cost { get; set; }
+        public long receipt_id { get; set; }
+        public int listing_id { get; set; }
+        public string transaction_type { get; set; }
+    }
+
+
+    public class EtsySale
+    {
+        public int count { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+    public class SaleCount
+    {
+        public int count { get; set; }
+    }
+
+
+
+
+
+}
---
+++ EtsyBusinessMetricsAPI/Models/EtsyReceipt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EtsyBusinessMetricsAPI.Models
+{
+
+
+
+
+    public class Receipts
+    {
+        public int receipt_id { get; set; }
+        //public string first_line { get; set; }
+        //public string second_line { get; set; }
+        //public string city { get; set; }
+        //public string state { get; set; }
+        //public string zip { get; set; }
+        //public string formatted_address { get; set; }
+        public string message_from_seller { get; set; }
+        public string message_from_buyer { get; set; }
+        public bool was_paid { get; set; }
+        public string total_tax_cost { get; set; }
+        public string total_price { get; set; }
+        public string total_shipping_cost { get; set; }
+        public string currency_code { get; set; }
+        public string discount_amt { get; set; }
+        public string etsy_coupon_discount_amt { get; set; }
+        public string subtotal { get; set; }
+        public string grandtotal { get; set; }
+        public string adjusted_grandtotal { get; set; }
+        public int shipped_date { get; set; }
+        public bool is_overdue { get; set; }
+        public int days_from_due_date { get; set; }
+
+    }
+
+
+    public class EtsyReceipt
+    {
+        public int count { get; set; }
+        public List<Receipts> results { get; set; }
+    }
+
+}
